c6c6b4227948500b0cd51c259ceb2f3c692738fe
Carl Bennett <carl@carlbennett.me>
2019-01-16 22:45:40 -0600

    Add alternative compile line to build script

--- build.sh
+++ build.sh
@@ -7,5 +7,9 @@ find -name '*.java' > build.sources.tmp
 javac @build.sources.tmp
 rm build.sources.tmp
 
+# Alternative compile: not sure how different this is from the "official" way,
+# but it seems cleaner and seems to produce the same output.
+#find -name '*.java' -print0 | xargs -0 javac
+
 # Create archive
 jar cfm ./JBLS-$(git log --pretty=format:'%h' -n 1).jar ./MANIFEST.MF *
